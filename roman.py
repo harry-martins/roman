1e9d3bd8c36858beb5685ffa98b796256a386a4e
harry-martins <52189030+harry-martins@users.noreply.github.com>
2019-08-14 12:41:21 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,41 @@
+n=input()
+if(n=="I"):
+	print("1")
+if(n=="II"):
+	print("2")
+if(n=="III"):
+	print("3")
+if(n=="IV"):
+	print("4")
+if(n=="V"):
+	print("5")
+if(n=="VI"):
+	print("6")
+if(n=="VII"):
+	print("7")
+if(n=="VIII"):
+	print("8")
+if(n=="IX"):
+	print("9")
+if(n=="X"):
+	print("10")
+if(n=="XI"):
+	print("11")
+if(n=="XII"):
+	print("12")
+if(n=="XIII"):
+	print("13")
+if(n=="XIV"):
+	print("14")
+if(n=="XV"):
+	print("15")
+if(n=="XVI"):
+	print("16")
+if(n=="XVII"):
+	print("17")
+if(n=="XVIII"):
+	print("18")
+if(n=="XIX"):
+	print("19")
+if(n=="XX"):
+	print("20")
